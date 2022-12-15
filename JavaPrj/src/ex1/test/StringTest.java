cd85a7a0f70c2cec0b9308080bbf4d3b3a5fd378
ict05-18 <ict05-18@DESKTOP-6TV2673>
2022-12-15 18:13:17 +0900

    2

--- JavaPrj/src/ex1/test/StringTest.java
+++ JavaPrj/src/ex1/test/StringTest.java
@@ -8,15 +8,15 @@ public class StringTest {
 		 String str2 = "sdfsdf";
 		 String str3 = new String("sdfsdf");
 		 
-		 if (str == str2) {
-			System.out.println("같 1");
-		}
-		 if (str == str3) {
-			 System.out.println("같 2");
-		}
-		 if (str.equals(str3)) {
-			 System.out.println("같 3");
-		}
+//		 if (str == str2) {
+//			System.out.println("같 1");
+//		}
+//		 if (str == str3) {
+//			 System.out.println("같 2");
+//		}
+//		 if (str.equals(str3)) {
+//			 System.out.println("같 3");
+//		}
 
 	}
 
